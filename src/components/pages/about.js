97ed7b8610aac842bf62aaacef979c2c54130ac3
Your Name Comes Here <pseudorandomsw@gmail.com>
2020-08-15 22:30:22 -0600

    added everything else (blogs, about, contact, portfolio, etc.)

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React from "react";
+import profilePicture from "../../../static/assets/images/bio/warren-wong-4l-e7U6c5ek-unsplash.jpg";
 
-export default function() {
-    return (
-        <div>
-            About
-        </div>
-    );
+export default function () {
+  return (
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          backgroundImage: `url(${profilePicture})`,
+        }}
+      />
+
+      <div className="right-column">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam facilisis,
+        magna vitae fermentum maximus, orci tellus aliquet lorem, nec gravida
+        nisi ipsum non mi. Integer fringilla arcu magna, in mattis lectus
+        porttitor non. Morbi vel sodales sem. Sed interdum elit lorem, id
+        vulputate ex egestas non. Mauris vel ultricies urna, non pharetra ante.
+        Sed ultricies consectetur magna sit amet aliquet. Sed at libero magna.
+        Nunc lacinia, dolor eget sollicitudin placerat, lacus nisl vulputate
+        lectus, ac condimentum nibh felis at sem. Fusce a est nisl. Quisque
+        pellentesque, turpis a lacinia scelerisque, sem dolor pulvinar diam, id
+        consequat nibh ante eget orci. Nam blandit nibh eget purus hendrerit, id
+        suscipit tortor pretium.
+      </div>
+    </div>
+  );
 }
--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import contactPicture from "../../../static/assets/images/contact/vladislav-klapin-SymZoeE8quA-unsplash.jpg";
 
-export default function() {
-    return (
-        <div>
-            Contact
+export default function () {
+  return (
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          backgroundImage: `url(${contactPicture})`,
+        }}
+      />
+
+      <div className="right-column">
+        <div className="contact-bullet-points">
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="phone" />
+            </div>
+            <div className="text">555-555-5555</div>
+          </div>
+
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="envelope" />
+            </div>
+            <div className="text">jordant@example.com</div>
+          </div>
+
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="map-marked-alt" />
+            </div>
+            <div className="text">City, UT</div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
